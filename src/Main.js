32a51ecd4bd8f5a69a8b1fd04e27ac3537fbcf61
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-12 14:20:10 -0500

    Removed HTML added components

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,9 @@
+import Hero from "./components/Hero/Hero"
+
 function App() {
   return (
     <main className="App">
-        <section>
-            <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
-            <p>We are a family owned Mediterranean restaurant, focused of traditional recipes with a modern twist</p>
-        </section>
+        <Hero />
     </main>
   );
 }
