24a61a27df47d5a0ebc9d304054de4752ef09281
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-12 14:16:18 -0500

    Made changes to layout

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import "./Hero.css"
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="container hero-content">
+        <div className="hero-text">
+            <h1 className="title">Little Lemon</h1>
+            <h2 className="subtitle">Chicago</h2>
+            <p>Nestled in the heart of bustling Chicago, Little Lemon is a family owned Mediterranean restaurant focused on traditional recipes with a modern twist. We invite you to experience our diverse, artisanal menu in a modern and accomodating space.</p>
+            <a className="primary-button" href="/">Reserve A Table</a>
+        </div>
+        <img src="/mario-adrian-hero.png" alt="Two chefs in a kitchen" />
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
