7edeb9be5537cde0ce5e885b9c640b07deab1df1
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-13 16:40:06 -0500

    updated Footer import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './Header.js'
-import Footer from './Footer.js'
+import Footer from './components/Footer/Footer.js'
 import Nav from './Nav.js'
 import Main from './Main.js'
 
