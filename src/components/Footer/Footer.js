a7bc1266f37b0b34f1542bfd94ca15ef814c7f41
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-16 15:36:25 -0500

    wrapped footer info in div

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,48 @@
 import './Footer.css';
+import Logo from '../assets/little-lemon-secondary.svg';
 
 function Footer() {
-  return (
-    <footer>
-        <div className="container footer-nav">
-            <img src='/little-lemon-secondary.svg' alt="Logo"/>
-            <div className="footer-links">
-                <div>
-                    <h3>Little Lemon Chicago</h3>
-                    <address className="contact-info">
-                        <p>123 Main St</p>
-                        <p>Chicago, IL 60657</p>
-                        <p>Phone: (312) 555-1234</p>
-                    </address>
+    return (
+        <footer>
+            <div className="container footer-nav">
+
+                <img src={Logo} alt="Little Lemon logo" />
+
+                <div className='footer-content'>
+                    <div>
+                        <h3>Little Lemon Restaurant</h3>
+                        <address>
+                            <p>123 Main St</p>
+                            <p>Chicago, IL 60657</p>
+                            <div className="contact-info">
+                                <p><strong>P:</strong> (312) 555-1234</p>
+                                <p><strong>E:</strong> contact@littlelemon.com</p>
+                            </div>
+                        </address>
+                    </div>
+                    <div>
+                        <h3>Navigation</h3>
+                        <ul>
+                            <li><a href="/">About</a></li>
+                            <li><a href="/">Menu</a></li>
+                            <li><a href="/">Reservations</a></li>
+                            <li><a href="/">Order</a></li>
+                            <li><a href="/">Login</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Social</h3>
+                        <ul>
+                            <li><a href="/">Facebook</a></li>
+                            <li><a href="/">Instagram</a></li>
+                            <li><a href="/">X</a></li>
+                        </ul>
+                    </div>
                 </div>
-                <div>
-                    <h3>Navigation</h3>
-                    <ul>
-                        <li><a href="/">About</a></li>
-                        <li><a href="/">Menu</a></li>
-                        <li><a href="/">Reservations</a></li>
-                        <li><a href="/">Order</a></li>
-                        <li><a href="/">Login</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Social</h3>
-                    <ul>
-                        <li><a href="/">Facebook</a></li>
-                        <li><a href="/">Instagram</a></li>
-                        <li><a href="/">X</a></li>
-                    </ul>
-                </div>
+
             </div>
-            </div>
-    </footer>
-  );
+        </footer>
+    );
 }
 
 export default Footer;
