d738e7af8c9db1e1416b33b934fbd8d158828182
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-16 16:43:40 -0500

    removed nav elements to new file structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './Header.js'
 import Footer from './components/Footer/Footer.js'
-import Nav from './Nav.js'
+import Nav from './components/Nav/Nav.js'
 import Main from './Main.js'
 
 function App() {
