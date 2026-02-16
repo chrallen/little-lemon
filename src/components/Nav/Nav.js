8597ab49ee62e450a2b0a473f54af50a60b9ff6d
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-16 16:43:08 -0500

    new file structure

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import './Nav.css';
+
+function App() {
+  return (
+    <nav>
+        <div className="container navigation">
+            <img src="/little-lemon.svg" alt="Logo" />
+            <ul className="nav-links">
+                <li><a className="text-link" href="/">About</a></li>
+                <li><a className="text-link" href="/">Menu</a></li>
+                <li><a className="text-link" href="/">Reservations</a></li>
+                <li><a className="text-link" href="/">Order</a></li>
+                <li><a className="text-link" href="/">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default App;
