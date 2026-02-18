0c685bf9c7620f40ea9b5e3b8cf29a0ee7566557
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-18 16:22:12 -0500

    added specials component

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero/Hero"
+import Specials from "./components/Specials/Specials";
 
 function App() {
   return (
     <main className="App">
         <Hero />
+        <Specials />
     </main>
   );
 }
