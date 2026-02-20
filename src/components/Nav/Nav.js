cd6c9efc6dc9c56b53cc4096020615bd6c0b8bd8
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-20 10:07:09 -0500

    added menu icon to mobile layout

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,15 @@
+import { useState, useEffect } from 'react';
 import './Nav.css';
 import Logo from '../../assets/images/little-lemon.svg';
+import MenuIcon from '../../assets/icons/menu.svg'
 
 function App() {
+	const [menuOpen, setMenuOpen] = useState(false);
+
+	useEffect (() => {
+		document.body.style.overflow = menuOpen ? 'hidden' : 'auto';
+	}, [menuOpen]);
+
 	return (
 		<nav>
 			<div className="container navigation">
@@ -12,7 +20,15 @@ function App() {
 					className='nav-logo'
 				/>
 
-				<ul className="nav-links">
+				<button
+					className='menu-toggle'
+					onClick={() => setMenuOpen(!menuOpen)}
+					aria-label='Toggle menu'
+				>
+					<img src={MenuIcon} alt='Menu'/>
+				</button>
+
+				<ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
 					<li><a className="text-link" href="/">About</a></li>
 					<li><a className="text-link" href="/">Menu</a></li>
 					<li><a className="text-link" href="/">Reservations</a></li>
