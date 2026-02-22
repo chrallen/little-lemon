8f47cc0997aa5c4214dd0e2ad0c72be2586488ff
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-21 21:21:06 -0500

    Added Reviews component

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero"
 import Specials from "./components/Specials/Specials";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
     <main className="App">
         <Hero />
         <Specials />
+        <Reviews />
     </main>
   );
 }
