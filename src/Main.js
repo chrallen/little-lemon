192f1f4e016b83fa3f14386881b82dadd2ce7419
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-23 15:19:20 -0500

    added about

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero/Hero"
 import Specials from "./components/Specials/Specials";
 import Reviews from "./components/Reviews/Reviews";
+import About from "./components/About/About"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Hero />
         <Specials />
         <Reviews />
+        <About />
     </main>
   );
 }
