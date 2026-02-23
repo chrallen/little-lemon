1ad505648db73c09b3fa23609cf19d34e04e1128
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-23 09:30:33 -0500

    Rating feature added

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,18 +1,20 @@
 import './ReviewCard.css'
+import rating from '../../assets/images/rating.svg'
 
 function ReviewCard({ image, name, description }) {
   return (
     <article className="review-card">
 
 
-      <div className='card-content'>
-          <div className="card-header">
+      <div className='review-card-content'>
+          <div className="review-card-header">
             <img src={image} alt={name} />
             <h3>{name}</h3>
           </div>
 
           {/*Star Rating Here */}
-          
+          <img src={rating} alt='star rating' className='rating'/>
+
           <p className="description">{description}</p>
       </div>
     </article>
