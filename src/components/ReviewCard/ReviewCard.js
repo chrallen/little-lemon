921a53f8a8b10874465c8600841c9175a2193bc4
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-23 10:53:25 -0500

    added rating

--- src/components/ReviewCard/ReviewCard.js
+++ src/components/ReviewCard/ReviewCard.js
@@ -1,19 +1,29 @@
 import './ReviewCard.css'
-import rating from '../../assets/images/rating.svg'
+import star from '../../assets/icons/star.svg'
 
-function ReviewCard({ image, name, description }) {
+function ReviewCard({ image, name, description, rating }) {
   return (
     <article className="review-card">
-
-
       <div className='review-card-content'>
           <div className="review-card-header">
             <img src={image} alt={name} />
             <h3>{name}</h3>
           </div>
 
-          {/*Star Rating Here */}
+          {/*Star Rating Here
           <img src={rating} alt='star rating' className='rating'/>
+          */}
+
+          <div className='rating'>
+            {Array.from({ length: 5 }, (_, index) => (
+              <img
+                key={index}
+                src={star}
+                alt='star'
+                className={index < rating ? "star filled" : "star"}
+              />
+            ))}
+          </div>
 
           <p className="description">{description}</p>
       </div>
