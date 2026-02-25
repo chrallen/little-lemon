ad88ed860c76d93d16aa9bea0d2215278e31094c
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-25 11:00:17 -0500

    updated function name and two-column layouts

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,17 +2,17 @@ import "./About.css"
 import chefs from "../../assets/images/marioAdrian.jpg"
 import restaurant from "../../assets/images/restaurant.jpg"
 
-function About() {
+const About = () => {
   return (
-    <section className="about">
-      <div className="container about-content">
-        <div className="about-text">
+    <section className="about two-column-section">
+      <div className="container two-column-content">
+        <div className="section-text">
             <h1 className="title">Our Story</h1>
             <p>At Little Lemon, chefs Mario and Adrian bring traditional Mediterranean recipes with a creative twist that stands out from the rest. Whether you're looking for a quick lunch or a leisurely dinner, Little Lemon Chicago is the perfect place to indulge in a delicious meal in a cozy and welcoming atmosphere.</p>
             <a className="primary-btn" href="/">Reserve A Table</a>
         </div>
 
-        <div className="about-images">
+        <div className="section-media about-images">
           <img src={chefs} alt="mario and adrian" className="img-front" />
           <img src={restaurant} alt="Little Lemon restaurant" className="img-back" />
         </div>
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,17 @@
 import "./Hero.css"
 import dish from "../../assets/images/dish.jpg"
 
-function Hero() {
+const Hero = () => {
   return (
-    <section className="hero">
-      <div className="container hero-content">
-        <div className="hero-text">
+    <section className="hero two-column-section">
+      <div className="container two-column-content">
+        <div className="section-text">
             <h1 className="title">Little Lemon</h1>
             <h2 className="subtitle">Chicago</h2>
             <p>Nestled in the heart of bustling Chicago, Little Lemon is a family owned Mediterranean restaurant focused on traditional recipes with a modern twist. We invite you to experience our diverse, artisanal menu in a modern and accomodating space.</p>
             <a className="primary-btn" href="/">Reserve A Table</a>
         </div>
-        <img src={dish} alt="people sharing food" />
+        <div className="section-media"><img src={dish} alt="people sharing food" /></div>
       </div>
     </section>
   );
