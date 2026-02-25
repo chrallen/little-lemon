c7da3482c920eeaab4f71d8626a4ee0b10385d3e
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-25 13:37:04 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav/Nav.js'
 import HomePage from './pages/HomePage.js'
+import AboutPage from './pages/AboutPage.js'
+import MenuPage from './pages/MenuPage.js'
+import ReservationPage from './pages/ReservationPage.js';
+import OrderPage from './pages/OrderPage.js'
+import LoginPage from './pages/LoginPage.js'
 
 const App = () => {
 	return (
@@ -11,6 +16,11 @@ const App = () => {
 
 				<Routes>
 					<Route path='/' element={<HomePage />} />
+					<Route path='AboutPage' element={<AboutPage />} />
+					<Route path='MenuPage' element={<MenuPage />} />
+					<Route path='ReservationPage' element={<ReservationPage />} />
+					<Route path='OrderPage' element={<OrderPage />} />
+					<Route path='LoginPage' element={<LoginPage />} />
 				</Routes>
 
 			</BrowserRouter>
