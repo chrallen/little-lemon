4ca2e07b883a8e1b001651eb013830a549231b90
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-02-26 15:40:35 -0500

    created UnderConstruction component and css styles

--- src/components/UnderConstruction/UnderConstruction.js
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -1,5 +1,18 @@
-import UnderConstruction from './UnderConstruction.css'
+import './UnderConstruction.css'
+import { Link } from 'react-router-dom'
 
-const UnderConstruction = () => {
-  
+const Message = () => {
+  return (
+    <section className='message'>
+      <div className='container'>
+        <div className='message-content'>
+          <h1 className='title'>This page is under construction</h1>
+          <p>Please come back soon!</p>
+          <Link to="/" className="primary-btn">Return Home</Link>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+export default Message
