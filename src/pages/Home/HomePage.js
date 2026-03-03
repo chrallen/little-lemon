99a64153bfdca4a2fbc8eb8d6636506458bfcda5
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-03-03 11:07:51 -0500

    removed semicolons

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,6 +1,6 @@
 import Hero from "../../components/Hero/Hero"
-import Specials from "../../components/Specials/Specials";
-import Reviews from "../../components/Reviews/Reviews";
+import Specials from "../../components/Specials/Specials"
+import Reviews from "../../components/Reviews/Reviews"
 import About from "../../components/About/About"
 
 const HomePage = () => {
