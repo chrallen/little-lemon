ac4fe7e32480cf2a3023fda8e2f63061c4357e2c
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-03-04 21:32:28 -0500

    added form and functionality

--- src/pages/Reservations/ReservationPage.js
+++ src/pages/Reservations/ReservationPage.js
@@ -1,11 +1,45 @@
-import UnderConstruction from "../../components/UnderConstruction/UnderConstruction"
+import './ReservationPage.css';
+import ReservationForm from '../../components/ReservationForm/ReservationForm';
+import { useReducer } from 'react';
+
+export const initializeTimes = () => {
+  return [
+    "5:00",
+    "6:00",
+    "7:00",
+    "8:00",
+    "9:00",
+  ];
+};
+
+export const  updateTimes = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_TIMES":
+      return initializeTimes();
+
+    default:
+      return state;
+  }
+};
 
 const ReservationPage = () => {
+  const [availableTimes, dispatch] = useReducer(
+    updateTimes,
+    [],
+    initializeTimes
+  );
+
   return (
-    <>
-      <UnderConstruction />
-    </>
-  )
+    <section className='single-column-center form'>
+      <h1 className='title'>Make a Reservation</h1>
+        <div className='form-content'>
+          <ReservationForm
+            availableTimes={availableTimes}
+            dispatch={dispatch}
+          />
+        </div>
+    </section>
+  );
 }
 
 export default ReservationPage;
