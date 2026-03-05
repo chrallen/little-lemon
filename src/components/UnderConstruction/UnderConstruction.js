1900e8e2c3b0a625c8b862f1a5115ab2bb651b33
Chris Allen <163036996+chrallen@users.noreply.github.com>
2026-03-04 20:33:30 -0500

    removed container class and simplified layout

--- src/components/UnderConstruction/UnderConstruction.js
+++ src/components/UnderConstruction/UnderConstruction.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Message = () => {
   return (
-    <section className='message'>
-      <div className='container'>
+    <section className='single-column-center message'>
         <div className='message-content'>
           <h1 className='title'>This page is under construction</h1>
           <p>Please come back soon!</p>
           <Link to="/" className="primary-btn">Return Home</Link>
         </div>
-      </div>
     </section>
   );
 }
